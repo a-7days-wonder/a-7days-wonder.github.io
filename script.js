11e07ae05159cf96ec8e30b984835148a1bccd32
Syunpei Nagata <n1510124@edu.cc.uec.ac.jp>
2018-02-28 00:26:14 +0900

    一旦完成

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function(){
+	var nav = $('#navbar');
+	var navTop = nav.offset().top;
+    //スクロールするたびに実行
+    $(window).scroll(function () {
+        var winTop = $(this).scrollTop();
+        //スクロール位置がnavの位置より下だったらクラスfixedを追加
+        if (winTop >= navTop) {
+            nav.addClass('fixed')
+        } else if (winTop <= navTop) {
+            nav.removeClass('fixed')
+        }
+    });
+});
